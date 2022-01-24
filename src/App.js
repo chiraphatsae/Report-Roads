07f7b3366008a414db08055eeb74169cb0e9b2bb
chiraphat worra <chirapat.sae@gmail.com>
2022-01-25 01:41:22 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Sidebar from "./component/sidebar/Sidebar";
+import Topbar from "./component/topbar/Topbar";
+import './app.css'
+
+function App() {
+  return (
+    <div >
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <div className="others">other pages</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/topbar/Topbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './topbar.css'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import LanguageIcon from '@mui/icons-material/Language';
+import SettingsIcon from '@mui/icons-material/Settings';
+const Topbar = () => {
+    return (
+        <div className='topbar '>
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <div className="logo">Mui</div>
+                   
+                </div>
+               
+                <div className="topRight">
+                    <div className="topbarIconsContainer">
+                        <NotificationsNoneIcon/>
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconsContainer ">
+                        <LanguageIcon/>
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconsContainer">
+                        <SettingsIcon/>
+                       
+                    </div>
+                    <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Topbar;
